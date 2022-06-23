79681ac57120165f89db82a0c3455069117d7cf3
FredyVizcarra <fredyvizcarrag@gmail.com>
2022-06-23 02:35:22 -0500

    Adding hooks and request to API

--- react-shop/src/components/Menu.jsx
+++ react-shop/src/components/Menu.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import '../styles/Menu.scss';
+import '@styles/Menu.scss';
 
 const Menu = () => {
     return (
-        <div className="desktop-menu">
+        <div className="Menu">
             <ul>
                 <li>
                     <a href="/" className="title">My orders</a>
                 </li>
-
                 <li>
                     <a href="/">My account</a>
                 </li>
-
                 <li>
                     <a href="/">Sign out</a>
                 </li>
